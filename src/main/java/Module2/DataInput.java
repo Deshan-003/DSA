d73013abbf88cad646cde706d6cb594c3e171041
Produlx <dulsathlenovo12@gmail.com>
2026-02-26 22:07:05 +0530

    Create new class

--- src/main/java/Module2/DataInput.java
+++ src/main/java/Module2/DataInput.java
@@ -1,37 +1,4 @@
-import java.util.*;
+package Module2;
 
 public class DataInput {
-
-    //Manual inputting..
-    public static int[] manualInput(Scanner sc) {
-        system.out.println("Type number of elements: ");
-        int n =sc.nextInt();
-
-        int[] arr = new int[n];
-
-        System.out.println("Type the numbers: ");
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        return arr;
-    }
-
-    //Random inputting
-    public static int[] randomInput(Scanner sc) {
-        System.out.print("Enter number of elements: ");
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-        Random rand = new Random();
-
-        for(int i = 0; i < n; i++) {
-            arr[i] = rand.nextInt(1000);
-        }
-
-        System.out.println("Random numbers generated.");
-        return arr;
-
-    }
-
-
 }
