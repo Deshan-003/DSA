3410150d59248d903ffa348cedec4e2e63792fc8
Produlx <dulsathlenovo12@gmail.com>
2026-02-26 22:11:53 +0530

    added manual inputting section to the DataInput class

--- src/main/java/Module2/DataInput.java
+++ src/main/java/Module2/DataInput.java
@@ -1,4 +1,25 @@
 package Module2;
 
+import java.util.*;
+
 public class DataInput {
+
+    // Manual inputting
+    public static int[] manualInput(Scanner sc) {
+        System.out.print("Type number of elements: ");
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Type the numbers:");
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        return arr;
+    }
+
+
 }
