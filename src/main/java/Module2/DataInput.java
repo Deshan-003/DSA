13f813fdf83e9fc6ee7756e14cc20117141c5f38
Produlx <dulsathlenovo12@gmail.com>
2026-02-26 22:16:36 +0530

    added random inputting section to the DataInput class

--- src/main/java/Module2/DataInput.java
+++ src/main/java/Module2/DataInput.java
@@ -12,11 +12,33 @@ public class DataInput {
 
         int[] arr = new int[n];
 
+        System.out.print("");
         System.out.println("Type the numbers:");
 
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+
+        return arr;
+    }
+
+
+
+    //Random inputting
+    public static int[] randomInput(Scanner sc) {
+        System.out.print("Enter number of elements: ");
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        Random rand = new Random();
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = rand.nextInt(1000);
+        }
+
+        System.out.print("");
+        System.out.println("Random numbers generated...");
 
         return arr;
     }
