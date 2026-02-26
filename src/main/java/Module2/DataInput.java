7e137f7c2f95d8571e244baeb9ca9c328ecf1011
Produlx <dulsathlenovo12@gmail.com>
2026-02-26 20:35:55 +0530

    Add random input to it

--- src/main/java/Module2/DataInput.java
+++ src/main/java/Module2/DataInput.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class DataInput {
 
-    //Manual inputing..
+    //Manual inputting..
     public static int[] manualInput(Scanner sc) {
         system.out.println("Type number of elements: ");
         int n =sc.nextInt();
@@ -15,4 +15,23 @@ public class DataInput {
         }
         return arr;
     }
+
+    //Random inputting
+    public static int[] randomInput(Scanner sc) {
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        Random rand = new Random();
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = rand.nextInt(1000);
+        }
+
+        System.out.println("Random numbers generated.");
+        return arr;
+
+    }
+
+
 }
