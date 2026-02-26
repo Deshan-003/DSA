0a80450bd8c36398ddc95c28a721372bbd470bca
Deshan Ganganath <hdganganath003@gmail.com>
2026-02-26 19:53:44 +0530

    create a bst

--- src/main/java/Module3/BinarySearchTree.java
+++ src/main/java/Module3/BinarySearchTree.java
@@ -2,6 +2,14 @@ package Module3;
 
 public class BinarySearchTree {
 
+    class node{
+        int data;
+        node left, right;
 
+        node(int value) {
+            data = value;
+            left = right = null;
+        }
+    }
 
 }
