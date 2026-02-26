8642d3d0e56575c02dc38828bae2c702ac1b1c47
sethullovidu <sethullovidu77@gmail.com>
2026-02-26 22:08:47 +0530

    implemnting Binary search tree
    # Conflicts:
    #	src/main/java/Module1/Graph.java
    #	src/main/java/RoutePlannerModule.java

--- src/main/java/Module1/BST.java
+++ src/main/java/Module1/BST.java
@@ -1,4 +1,35 @@
 package Module1;
 
 public class BST {
+
+
+    private static class BSTNode{
+        String locationName;
+        BSTNode left, right;
+
+        BSTNode(String name) {
+            this.locationName = name;
+            this.left = null;
+            this.right = null;
+        }
+        private BSTNode root;
+
+
+        public BST() {
+            root = null;
+        }
+        public void insert(String name) {
+            root = insertRec(root, name);
+        }
+
+
+
+
+
+
+
+    }
+
+
+
 }
