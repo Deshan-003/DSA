e08ba36fccd9289cbd827e268cd7535e5f098d75
Produlx <dulsathlenovo12@gmail.com>
2026-02-26 22:31:51 +0530

    now user can choose Random or manual , testing numbers (Random and inputting)

--- src/main/java/Module2/TestInput.java
+++ src/main/java/Module2/TestInput.java
@@ -1,4 +1,30 @@
 package Module2;
 
+import java.util.*;
+
 public class TestInput {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] numbers;
+
+        System.out.println("1. Enter Numbers");
+        System.out.println("2. Generate Random Numbers");
+
+        System.out.print("Choose option: ");
+        int choice = sc.nextInt();
+
+        if(choice == 1) {
+            numbers = DataInput.manualInput(sc);
+        }
+        else {
+            numbers = DataInput.randomInput(sc);
+        }
+
+        System.out.println("Numbers are:");
+        for(int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
 }
