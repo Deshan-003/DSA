619235c925510045af29ff877c9263e402e423e2
Produlx <dulsathlenovo12@gmail.com>
2026-02-26 20:53:18 +0530

    user interface about choosing options and visible numbers

--- src/main/java/Module2/TestInput.java
+++ src/main/java/Module2/TestInput.java
@@ -1,1 +1,28 @@
 import java.util.*;
+
+public class TestInput {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] numbers;
+
+        System.out.println("1. Enter Numbers");
+        System.out.println("2. Generate Random Numbers");
+
+        System.out.print("Choose option: ");
+        int choice = sc.nextInt();
+
+        if(choice == 1) {
+            numbers = DataInput.manualInput(sc);
+        }
+        else {
+            numbers = DataInput.randomInput(sc);
+        }
+
+        System.out.println("Numbers are:");
+        for(int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
