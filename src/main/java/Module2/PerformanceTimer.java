0bcc19915e9ace6b39c1acaa884207c2b34846d3
Produlx <dulsathlenovo12@gmail.com>
2026-02-26 23:59:18 +0530

    completed the performance timer.

--- src/main/java/Module2/PerformanceTimer.java
+++ src/main/java/Module2/PerformanceTimer.java
@@ -1,4 +1,37 @@
 package Module2;
 
 public class PerformanceTimer {
+
+    public static long measureBubble(int[] arr) {
+
+        int[] copy = arr.clone();
+
+        long start = System.nanoTime();
+        BubbleSort.sort(copy);
+        long end = System.nanoTime();
+
+        return end - start;
+    }
+
+    public static long measureMerge(int[] arr) {
+
+        int[] copy = arr.clone();
+
+        long start = System.nanoTime();
+        MergeSort.sort(copy, 0, copy.length - 1);
+        long end = System.nanoTime();
+
+        return end - start;
+    }
+
+    public static long measureQuick(int[] arr) {
+
+        int[] copy = arr.clone();
+
+        long start = System.nanoTime();
+        QuickSort.sort(copy, 0, copy.length - 1);
+        long end = System.nanoTime();
+
+        return end - start;
+    }
 }
