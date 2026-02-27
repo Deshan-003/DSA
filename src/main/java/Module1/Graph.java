668cb87a1b91fe217a18ef5ae7356149f263a69c
sethullovidu <sethullovidu77@gmail.com>
2026-02-27 23:39:17 +0530

    implemnting Graph
    # Conflicts:
    #	src/main/java/Module1/Graph.java
    #	src/main/java/RoutePlannerModule.java

--- src/main/java/Module1/Graph.java
+++ src/main/java/Module1/Graph.java
@@ -1,5 +1,21 @@
 package Module1;
 import java.util.*;
 public class Graph {
+    private Map<String, List<String>> adjacencyList;
+    public Graph() {
+        adjacencyList = new LinkedHashMap<>();
+    }
+
+
+    public boolean addLocation(String name) {
+        if (adjacencyList.containsKey(name)) {
+            System.out.println("  [!] Location '" + name + "' already exists in the graph.");
+            return false;
+        }
+        adjacencyList.put(name, new ArrayList<>());
+        System.out.println("  [+] Location '" + name + "' added to graph.");
+        return true;
+    }
+
 
 }
