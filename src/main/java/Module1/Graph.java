d4f0cc7332b37f0fb7b821e877fedbdc8985d06f
sethullovidu <sethullovidu77@gmail.com>
2026-02-27 23:20:57 +0530

    implemnting Graph
    # Conflicts:
    #	src/main/java/Module1/Graph.java
    #	src/main/java/RoutePlannerModule.java

--- src/main/java/Module1/Graph.java
+++ src/main/java/Module1/Graph.java
@@ -1,4 +1,9 @@
 package Module1;
-
+import java.util.*;
 public class Graph {
+
+    private Map<String,List <String>> adjacencyList ;
+    public Graph(){
+        adjacencyList = new LinkedHashMap<>();
+    }
 }
