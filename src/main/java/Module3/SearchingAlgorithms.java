ad406fb4a7fc57064af99c193ce8839c90e1a9e4
Deshan Ganganath <hdganganath003@gmail.com>
2026-02-28 00:51:32 +0530

    linearsearching

--- src/main/java/Module3/SearchingAlgorithms.java
+++ src/main/java/Module3/SearchingAlgorithms.java
@@ -2,4 +2,13 @@ package Module3;
 
 public class SearchingAlgorithms {
 
+    public static int linearsearch(int [] arr, int key) {
+        for(int i=0; i< arr.length; i++ ) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
+
