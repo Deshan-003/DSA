2c0aa0e80386611cabc67fc5951d090cc1cee681
sethullovidu <sethullovidu77@gmail.com>
2026-02-28 23:27:15 +0530

    implenting main and route planner

--- src/main/java/Module1/GraphModuleMain.java
+++ src/main/java/Module1/GraphModuleMain.java
@@ -3,23 +3,6 @@ package Module1;
 import java.util.Scanner;
 public class GraphModuleMain {
 
-    public static void main (String[] args){
-        Scanner scanner =new Scanner(System.in);
-        int choice = -1;
 
-
-
-
-
-
-        while(choice=0){
-            System.out.println("=======MAIN MENU=======");
-            System.out.println("1. Moduel 1 Smart city");
-            System.out.println("2. Module 2 data sorter");
-            System.out.println("3. Module 3 Alogorithm ");
-
-
-        }
-    }
 
 }
--- src/main/java/Module1/RoutePlanner.java
+++ src/main/java/Module1/RoutePlanner.java
@@ -1,4 +1,25 @@
 package Module1;
-
+import java.util.Scanner;
 public class RoutePlanner {
+    private Graph graph;
+    private BST locationTree;
+    private Scanner scanner;
+
+
+    public RoutePlanner(Scanner scanner){
+        this.graph = new Graph();
+        this.locationTree = new BST();
+        this.scanner=scanner;
+    }
+
+    public void run(){
+        int choice =-1;
+
+        System.out.println("module 1 smart city route planner ");
+
+
+    }
+
+
+
 }
