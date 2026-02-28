31fb0a728aee465933cb57c8ff6181fad8c0ea00
sethullovidu <sethullovidu77@gmail.com>
2026-02-28 23:34:22 +0530

    implenting main and route planner creating the menu and switch cases

--- src/main/java/Module1/RoutePlanner.java
+++ src/main/java/Module1/RoutePlanner.java
@@ -17,6 +17,45 @@ public class RoutePlanner {
 
         System.out.println("module 1 smart city route planner ");
 
+//        loadSampleData();
+
+        while(choice !=0){
+            printMenu();
+            choice = getIntInput("enter your choice");
+
+            switch (choice) {
+                case 1:
+                    addLocation();
+                    break;
+                case 2:
+                    removeLocation();
+                    break;
+                case 3:
+                    addRoad();
+                    break;
+                case 4:
+                    removeRoad();
+                    break;
+                case 5:
+                    displayConnections();
+                    break;
+                case 6:
+                    bfsTraversal();
+                    break;
+                case 7:
+                    dfsTraversal();
+                    break;
+                case 8:
+                    displayBST();
+                    break;
+                case 0:
+                    System.out.println("\n  Returning to main menu...\n");
+                    break;
+                default:
+                    System.out.println("  [!] Invalid choice. Please try again.");
+            }
+
+        }
 
     }
 
