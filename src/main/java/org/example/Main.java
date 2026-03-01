898c1d99ea1897fde28468805744685f0dab8b34
Deshan Ganganath <hdganganath003@gmail.com>
2026-03-01 16:51:37 +0530

    menu driven

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int choice;
 
+        do{
+            System.out.println("-------------------------------------");
+            System.out.println("-------------Menu.....---------------");
+            System.out.println("-------------------------------------");
+            System.out.println("1. Module 1- Smart City Planner");
+            System.out.println("2. Module 2- Data Sorter(Sorting Algorithms)");
+            System.out.println("3. Module 3- Algorithm Performance Analyzer");
+            System.out.println("4. Exit");
+            System.out.println("Enter Your Choice: ");
+
+            choice = sc.nextInt();
+
+
+        }
+
 
 
     }
