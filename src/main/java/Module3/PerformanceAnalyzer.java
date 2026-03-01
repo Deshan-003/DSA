28e3c8e61dce1e6ca4e9755bef034973261b8886
Deshan Ganganath <hdganganath003@gmail.com>
2026-03-01 09:58:04 +0530

    Add generateArray method to create random test data

--- src/main/java/Module3/PerformanceAnalyzer.java
+++ src/main/java/Module3/PerformanceAnalyzer.java
@@ -5,7 +5,15 @@ import java.util.Random;
 
 public class PerformanceAnalyzer {
 
+    public static int[] ArrayCreate(int size) {
+        Random rand = new Random();
+        int[] arr = new int[size];
 
+        for (int i =0; i<size; i++) {
+            arr[i] = rand.nextInt(10000);
+        }
+        return arr;
+    }
 
 
 }
